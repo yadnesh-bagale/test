9cee694b3761681624f41a9cd6d3fe8c07ef05cd
Yadnesh Bagale <133531065+yadnesh-bagale@users.noreply.github.com>
2024-08-12 18:05:53 +0000

    prac 2 done

---
+++ js-test/practical2.js
@@ -0,0 +1,20 @@
+// let name = new String("Yadnesh")
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(`My name is ${name}`);
+// console.log(name.toLowerCase(0))
+// console.log(name.toUpperCase(0,3))
+let n99= 123.22
+let n44= 2846.66
+let n66= n99 + n44
+console.log(n66.toPrecision(5))
+let max = 10
+console.log((((Math.random())*(max) )+ 1).toPrecision(2)) ;
+let today = new Date()
+console.log(typeof new Date());
+console.log(today.getDay);
+
+let name = "   Yadnesh-Bagale   "
+console.log(name.split("-"));
+console.log(name.slice());
+console.log(name.substring(0,4));

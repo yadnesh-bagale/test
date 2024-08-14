27221a8a208c6b2336f89f2ab8f0117454644a05
Yadnesh Bagale <133531065+yadnesh-bagale@users.noreply.github.com>
2024-08-14 10:32:20 +0000

    next1

--- js-test/practical2.js
+++ js-test/practical2.js
@@ -4,17 +4,25 @@
 // console.log(`My name is ${name}`);
 // console.log(name.toLowerCase(0))
 // console.log(name.toUpperCase(0,3))
-let n99= 123.22
-let n44= 2846.66
-let n66= n99 + n44
-console.log(n66.toPrecision(5))
-let max = 10
-console.log((((Math.random())*(max) )+ 1).toPrecision(2)) ;
-let today = new Date()
-console.log(typeof new Date());
-console.log(today.getDay);
+// let n99= 123.22
+// let n44= 2846.66
+// let n66= n99 + n44
+// console.log(n66.toPrecision(5))
+// let max = 10
+// console.log((((Math.random())*(max) )+ 1).toPrecision(2)) ;
+// let today = new Date()
+// console.log(typeof new Date());
+// console.log(today.getDay);
 
-let name = "   Yadnesh-Bagale   "
-console.log(name.split("-"));
-console.log(name.slice());
-console.log(name.substring(0,4));
+// let name = "   Yadnesh-Bagale   "
+// console.log(name.split("-"));
+// console.log(name.slice());
+// console.log(name.substring(0,4));
+
+let hero1 = ["sm", "thor"]
+let hero2 = ["bm", "spm"]
+const array = [...hero1, ...hero2]
+console.log(array);
+console.log(Array.from({name: "Yadnesh"}));
+
+

ecceecba1319211a34572b66444a5dfbcc979e0d
Yadnesh Bagale <133531065+yadnesh-bagale@users.noreply.github.com>
2024-08-15 08:24:38 +0000

    new2

--- js-test/practical2.js
+++ js-test/practical2.js
@@ -19,10 +19,57 @@
 // console.log(name.slice());
 // console.log(name.substring(0,4));
 
-let hero1 = ["sm", "thor"]
+let hero1 = ["im", "thor"]
 let hero2 = ["bm", "spm"]
 const array = [...hero1, ...hero2]
 console.log(array);
 console.log(Array.from({name: "Yadnesh"}));
+console.log(array.length-1);
+
+console.log(array.slice(1,3));
+console.log(array.splice(1,3));
+console.log(array);
+
+const sym1 = Symbol("key1")
+
+const obj1 = {
+    name: "Yadnesh",
+    email: "y@gmail.com",
+    [sym1]: "key1"
+}
+console.log(obj1["name"]);
+console.log(obj1[sym1]);
+console.log(typeof obj1[sym1]);
+
+obj1.email= "ya@gmail.com"
+Object.freeze(obj1)
+obj1.email= "yad@gmail.com"
+console.log(obj1);
+
+const obj2= {
+    name:"nayan",
+    email: "n@gmail.com",
+}
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const user = [
+    {
+    name :"vijay",
+    email : "v@gmail.com",
+},
+    {
+    name : "yad",
+    email: "yadnesh@gmailcom",
+ },
+    {
+    name:"nayn" ,
+    email:"nayan@gmail.com",
+    }
+]
+
+console.log(user[1].name)
+console.log(obj1.hasOwnProperty("name"));
+console.log(obj1.hasOwnProperty("key1"));
 
 
